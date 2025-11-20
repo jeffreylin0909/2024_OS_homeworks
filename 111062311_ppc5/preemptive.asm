35e2d685ed5f870c064b4c29a2c9d90ae2ca6a0b
林哲宇 <56347663+jeffreylin0909@users.noreply.github.com>
2025-11-21 00:37:27 +0800

    recover missing

---
+++ 111062311_ppc5/preemptive.asm
@@ -0,0 +1,682 @@
+;--------------------------------------------------------
+; File Created by SDCC : free open source ANSI-C Compiler
+; Version 4.0.0 #11528 (Linux)
+;--------------------------------------------------------
+	.module preemptive
+	.optsdcc -mmcs51 --model-small
+	
+;--------------------------------------------------------
+; Public variables in this module
+;--------------------------------------------------------
+	.globl _Bootstrap
+	.globl _main
+	.globl _CY
+	.globl _AC
+	.globl _F0
+	.globl _RS1
+	.globl _RS0
+	.globl _OV
+	.globl _F1
+	.globl _P
+	.globl _PS
+	.globl _PT1
+	.globl _PX1
+	.globl _PT0
+	.globl _PX0
+	.globl _RD
+	.globl _WR
+	.globl _T1
+	.globl _T0
+	.globl _INT1
+	.globl _INT0
+	.globl _TXD
+	.globl _RXD
+	.globl _P3_7
+	.globl _P3_6
+	.globl _P3_5
+	.globl _P3_4
+	.globl _P3_3
+	.globl _P3_2
+	.globl _P3_1
+	.globl _P3_0
+	.globl _EA
+	.globl _ES
+	.globl _ET1
+	.globl _EX1
+	.globl _ET0
+	.globl _EX0
+	.globl _P2_7
+	.globl _P2_6
+	.globl _P2_5
+	.globl _P2_4
+	.globl _P2_3
+	.globl _P2_2
+	.globl _P2_1
+	.globl _P2_0
+	.globl _SM0
+	.globl _SM1
+	.globl _SM2
+	.globl _REN
+	.globl _TB8
+	.globl _RB8
+	.globl _TI
+	.globl _RI
+	.globl _P1_7
+	.globl _P1_6
+	.globl _P1_5
+	.globl _P1_4
+	.globl _P1_3
+	.globl _P1_2
+	.globl _P1_1
+	.globl _P1_0
+	.globl _TF1
+	.globl _TR1
+	.globl _TF0
+	.globl _TR0
+	.globl _IE1
+	.globl _IT1
+	.globl _IE0
+	.globl _IT0
+	.globl _P0_7
+	.globl _P0_6
+	.globl _P0_5
+	.globl _P0_4
+	.globl _P0_3
+	.globl _P0_2
+	.globl _P0_1
+	.globl _P0_0
+	.globl _B
+	.globl _ACC
+	.globl _PSW
+	.globl _IP
+	.globl _P3
+	.globl _IE
+	.globl _P2
+	.globl _SBUF
+	.globl _SCON
+	.globl _P1
+	.globl _TH1
+	.globl _TH0
+	.globl _TL1
+	.globl _TL0
+	.globl _TMOD
+	.globl _TCON
+	.globl _PCON
+	.globl _DPH
+	.globl _DPL
+	.globl _SP
+	.globl _P0
+	.globl _bitmap
+	.globl _tmp2
+	.globl _tmp1
+	.globl _tmp0
+	.globl _current_T
+	.globl _T_SP
+	.globl _ThreadCreate
+	.globl _ThreadYield
+	.globl _ThreadExit
+	.globl _myTimer0Handler
+;--------------------------------------------------------
+; special function registers
+;--------------------------------------------------------
+	.area RSEG    (ABS,DATA)
+	.org 0x0000
+_P0	=	0x0080
+_SP	=	0x0081
+_DPL	=	0x0082
+_DPH	=	0x0083
+_PCON	=	0x0087
+_TCON	=	0x0088
+_TMOD	=	0x0089
+_TL0	=	0x008a
+_TL1	=	0x008b
+_TH0	=	0x008c
+_TH1	=	0x008d
+_P1	=	0x0090
+_SCON	=	0x0098
+_SBUF	=	0x0099
+_P2	=	0x00a0
+_IE	=	0x00a8
+_P3	=	0x00b0
+_IP	=	0x00b8
+_PSW	=	0x00d0
+_ACC	=	0x00e0
+_B	=	0x00f0
+;--------------------------------------------------------
+; special function bits
+;--------------------------------------------------------
+	.area RSEG    (ABS,DATA)
+	.org 0x0000
+_P0_0	=	0x0080
+_P0_1	=	0x0081
+_P0_2	=	0x0082
+_P0_3	=	0x0083
+_P0_4	=	0x0084
+_P0_5	=	0x0085
+_P0_6	=	0x0086
+_P0_7	=	0x0087
+_IT0	=	0x0088
+_IE0	=	0x0089
+_IT1	=	0x008a
+_IE1	=	0x008b
+_TR0	=	0x008c
+_TF0	=	0x008d
+_TR1	=	0x008e
+_TF1	=	0x008f
+_P1_0	=	0x0090
+_P1_1	=	0x0091
+_P1_2	=	0x0092
+_P1_3	=	0x0093
+_P1_4	=	0x0094
+_P1_5	=	0x0095
+_P1_6	=	0x0096
+_P1_7	=	0x0097
+_RI	=	0x0098
+_TI	=	0x0099
+_RB8	=	0x009a
+_TB8	=	0x009b
+_REN	=	0x009c
+_SM2	=	0x009d
+_SM1	=	0x009e
+_SM0	=	0x009f
+_P2_0	=	0x00a0
+_P2_1	=	0x00a1
+_P2_2	=	0x00a2
+_P2_3	=	0x00a3
+_P2_4	=	0x00a4
+_P2_5	=	0x00a5
+_P2_6	=	0x00a6
+_P2_7	=	0x00a7
+_EX0	=	0x00a8
+_ET0	=	0x00a9
+_EX1	=	0x00aa
+_ET1	=	0x00ab
+_ES	=	0x00ac
+_EA	=	0x00af
+_P3_0	=	0x00b0
+_P3_1	=	0x00b1
+_P3_2	=	0x00b2
+_P3_3	=	0x00b3
+_P3_4	=	0x00b4
+_P3_5	=	0x00b5
+_P3_6	=	0x00b6
+_P3_7	=	0x00b7
+_RXD	=	0x00b0
+_TXD	=	0x00b1
+_INT0	=	0x00b2
+_INT1	=	0x00b3
+_T0	=	0x00b4
+_T1	=	0x00b5
+_WR	=	0x00b6
+_RD	=	0x00b7
+_PX0	=	0x00b8
+_PT0	=	0x00b9
+_PX1	=	0x00ba
+_PT1	=	0x00bb
+_PS	=	0x00bc
+_P	=	0x00d0
+_F1	=	0x00d1
+_OV	=	0x00d2
+_RS0	=	0x00d3
+_RS1	=	0x00d4
+_F0	=	0x00d5
+_AC	=	0x00d6
+_CY	=	0x00d7
+;--------------------------------------------------------
+; overlayable register banks
+;--------------------------------------------------------
+	.area REG_BANK_0	(REL,OVR,DATA)
+	.ds 8
+;--------------------------------------------------------
+; internal ram data
+;--------------------------------------------------------
+	.area DSEG    (DATA)
+_T_SP	=	0x0020
+_current_T	=	0x0024
+_tmp0	=	0x0025
+_tmp1	=	0x0026
+_tmp2	=	0x0027
+_bitmap	=	0x0028
+;--------------------------------------------------------
+; overlayable items in internal ram 
+;--------------------------------------------------------
+	.area	OSEG    (OVR,DATA)
+;--------------------------------------------------------
+; indirectly addressable internal ram data
+;--------------------------------------------------------
+	.area ISEG    (DATA)
+;--------------------------------------------------------
+; absolute internal ram data
+;--------------------------------------------------------
+	.area IABS    (ABS,DATA)
+	.area IABS    (ABS,DATA)
+;--------------------------------------------------------
+; bit data
+;--------------------------------------------------------
+	.area BSEG    (BIT)
+;--------------------------------------------------------
+; paged external ram data
+;--------------------------------------------------------
+	.area PSEG    (PAG,XDATA)
+;--------------------------------------------------------
+; external ram data
+;--------------------------------------------------------
+	.area XSEG    (XDATA)
+;--------------------------------------------------------
+; absolute external ram data
+;--------------------------------------------------------
+	.area XABS    (ABS,XDATA)
+;--------------------------------------------------------
+; external initialized ram data
+;--------------------------------------------------------
+	.area XISEG   (XDATA)
+	.area HOME    (CODE)
+	.area GSINIT0 (CODE)
+	.area GSINIT1 (CODE)
+	.area GSINIT2 (CODE)
+	.area GSINIT3 (CODE)
+	.area GSINIT4 (CODE)
+	.area GSINIT5 (CODE)
+	.area GSINIT  (CODE)
+	.area GSFINAL (CODE)
+	.area CSEG    (CODE)
+;--------------------------------------------------------
+; global & static initialisations
+;--------------------------------------------------------
+	.area HOME    (CODE)
+	.area GSINIT  (CODE)
+	.area GSFINAL (CODE)
+	.area GSINIT  (CODE)
+;--------------------------------------------------------
+; Home
+;--------------------------------------------------------
+	.area HOME    (CODE)
+	.area HOME    (CODE)
+;--------------------------------------------------------
+; code
+;--------------------------------------------------------
+	.area CSEG    (CODE)
+;------------------------------------------------------------
+;Allocation info for local variables in function 'Bootstrap'
+;------------------------------------------------------------
+;	preemptive.c:80: void Bootstrap(void)
+;	-----------------------------------------
+;	 function Bootstrap
+;	-----------------------------------------
+_Bootstrap:
+	ar7 = 0x07
+	ar6 = 0x06
+	ar5 = 0x05
+	ar4 = 0x04
+	ar3 = 0x03
+	ar2 = 0x02
+	ar1 = 0x01
+	ar0 = 0x00
+;	preemptive.c:94: T_SP[0] = 0x3F;
+	mov	_T_SP,#0x3f
+;	preemptive.c:95: T_SP[1] = 0x4F;
+	mov	(_T_SP + 0x0001),#0x4f
+;	preemptive.c:96: T_SP[2] = 0x5F;
+	mov	(_T_SP + 0x0002),#0x5f
+;	preemptive.c:97: T_SP[3] = 0x6F;
+	mov	(_T_SP + 0x0003),#0x6f
+;	preemptive.c:98: bitmap = 0b0000;
+	mov	_bitmap,#0x00
+;	preemptive.c:100: TMOD = 0;
+	mov	_TMOD,#0x00
+;	preemptive.c:101: IE = 0x82;//10000010
+	mov	_IE,#0x82
+;	preemptive.c:103: TR0 = 1;
+;	assignBit
+	setb	_TR0
+;	preemptive.c:105: current_T = ThreadCreate(main);
+	mov	dptr,#_main
+	lcall	_ThreadCreate
+	mov	_current_T,dpl
+;	preemptive.c:106: RESTORESTATE;
+	mov	a,_current_T
+	add	a,#_T_SP
+	mov	r1,a
+	mov	_SP,@r1
+	POP PSW 
+	POP DPH 
+	POP DPL 
+	POP B 
+	POP A 
+;	preemptive.c:107: return;
+;	preemptive.c:108: }
+	ret
+;------------------------------------------------------------
+;Allocation info for local variables in function 'ThreadCreate'
+;------------------------------------------------------------
+;fp                        Allocated to registers 
+;------------------------------------------------------------
+;	preemptive.c:116: ThreadID ThreadCreate(FunctionPtr fp)
+;	-----------------------------------------
+;	 function ThreadCreate
+;	-----------------------------------------
+_ThreadCreate:
+;	preemptive.c:118: EA = 0;
+;	assignBit
+	clr	_EA
+;	preemptive.c:124: tmp2 = 0;
+	mov	_tmp2,#0x00
+;	preemptive.c:125: do
+00105$:
+;	preemptive.c:127: if (tmp2==4){
+	mov	a,#0x04
+	cjne	a,_tmp2,00129$
+	sjmp	00107$
+00129$:
+;	preemptive.c:130: if (!(bitmap&(1<<tmp2))){
+	mov	r7,_tmp2
+	mov	b,r7
+	inc	b
+	mov	r7,#0x01
+	mov	r6,#0x00
+	sjmp	00131$
+00130$:
+	mov	a,r7
+	add	a,r7
+	mov	r7,a
+	mov	a,r6
+	rlc	a
+	mov	r6,a
+00131$:
+	djnz	b,00130$
+	mov	r4,_bitmap
+	mov	r5,#0x00
+	mov	a,r4
+	anl	ar7,a
+	mov	a,r5
+	anl	ar6,a
+	mov	a,r7
+	orl	a,r6
+	jz	00107$
+;	preemptive.c:133: tmp2++;
+	mov	a,_tmp2
+	inc	a
+	mov	_tmp2,a
+;	preemptive.c:134: } while (1);
+	sjmp	00105$
+00107$:
+;	preemptive.c:136: if (tmp2 == 4){
+	mov	a,#0x04
+	cjne	a,_tmp2,00133$
+	sjmp	00110$
+00133$:
+;	preemptive.c:179: bitmap = bitmap|(1<<tmp2);
+	mov	r7,_tmp2
+	mov	b,r7
+	inc	b
+	mov	a,#0x01
+	sjmp	00136$
+00134$:
+	add	a,acc
+00136$:
+	djnz	b,00134$
+	mov	r7,a
+	mov	r6,_bitmap
+	orl	a,r6
+	mov	_bitmap,a
+;	preemptive.c:180: tmp0 = SP;
+	mov	_tmp0,_SP
+;	preemptive.c:181: SP = T_SP[tmp2];
+	mov	a,_tmp2
+	add	a,#_T_SP
+	mov	r1,a
+	mov	_SP,@r1
+;	preemptive.c:185: __endasm;
+	PUSH	DPL
+	PUSH	DPH
+;	preemptive.c:187: ACC = 0;
+	mov	_ACC,#0x00
+;	preemptive.c:193: __endasm;
+	PUSH	ACC
+	PUSH	ACC
+	PUSH	ACC
+	PUSH	ACC
+;	preemptive.c:195: tmp1 = tmp2<<3;
+	mov	a,_tmp2
+	mov	r7,a
+	swap	a
+	rr	a
+	anl	a,#0xf8
+	mov	_tmp1,a
+;	preemptive.c:198: __endasm;
+	PUSH	0x26;
+;	preemptive.c:200: T_SP[tmp2] = SP;
+	mov	a,_tmp2
+	add	a,#_T_SP
+	mov	r0,a
+	mov	@r0,_SP
+;	preemptive.c:201: SP = tmp0;
+	mov	_SP,_tmp0
+00110$:
+;	preemptive.c:205: EA = 1;
+;	assignBit
+	setb	_EA
+;	preemptive.c:206: return tmp2;
+	mov	dpl,_tmp2
+;	preemptive.c:207: }
+	ret
+;------------------------------------------------------------
+;Allocation info for local variables in function 'ThreadYield'
+;------------------------------------------------------------
+;	preemptive.c:216: void ThreadYield(void)
+;	-----------------------------------------
+;	 function ThreadYield
+;	-----------------------------------------
+_ThreadYield:
+;	preemptive.c:218: EA = 0;
+;	assignBit
+	clr	_EA
+;	preemptive.c:219: SAVESTATE;
+	PUSH A 
+	PUSH B 
+	PUSH DPL 
+	PUSH DPH 
+	PUSH PSW 
+	mov	a,_current_T
+	add	a,#_T_SP
+	mov	r0,a
+	mov	@r0,_SP
+;	preemptive.c:220: tmp0 = 1<<current_T;
+	mov	r7,_current_T
+	mov	b,r7
+	inc	b
+	mov	a,#0x01
+	sjmp	00124$
+00122$:
+	add	a,acc
+00124$:
+	djnz	b,00122$
+	mov	_tmp0,a
+;	preemptive.c:221: do
+00105$:
+;	preemptive.c:233: current_T++;
+	mov	a,_current_T
+	inc	a
+	mov	_current_T,a
+;	preemptive.c:234: tmp0 <<= 1;
+	mov	a,_tmp0
+	mov	r7,a
+	add	a,acc
+	mov	_tmp0,a
+;	preemptive.c:235: if (current_T==4){
+	mov	a,#0x04
+	cjne	a,_current_T,00102$
+;	preemptive.c:236: current_T = 0;
+	mov	_current_T,#0x00
+;	preemptive.c:237: tmp0 = 1;
+	mov	_tmp0,#0x01
+00102$:
+;	preemptive.c:239: if (bitmap&tmp0){
+	mov	a,_tmp0
+	anl	a,_bitmap
+	jz	00105$
+;	preemptive.c:243: RESTORESTATE;
+	mov	a,_current_T
+	add	a,#_T_SP
+	mov	r1,a
+	mov	_SP,@r1
+	POP PSW 
+	POP DPH 
+	POP DPL 
+	POP B 
+	POP A 
+;	preemptive.c:244: EA = 1;
+;	assignBit
+	setb	_EA
+;	preemptive.c:245: }
+	ret
+;------------------------------------------------------------
+;Allocation info for local variables in function 'ThreadExit'
+;------------------------------------------------------------
+;	preemptive.c:252: void ThreadExit(void)
+;	-----------------------------------------
+;	 function ThreadExit
+;	-----------------------------------------
+_ThreadExit:
+;	preemptive.c:254: EA = 0;
+;	assignBit
+	clr	_EA
+;	preemptive.c:262: bitmap = bitmap&(~(1<<current_T));
+	mov	r7,_current_T
+	mov	b,r7
+	inc	b
+	mov	a,#0x01
+	sjmp	00124$
+00122$:
+	add	a,acc
+00124$:
+	djnz	b,00122$
+	cpl	a
+	mov	r6,_bitmap
+	anl	a,r6
+	mov	_bitmap,a
+;	preemptive.c:263: tmp0 = 1<<current_T;
+	mov	r7,_current_T
+	mov	b,r7
+	inc	b
+	mov	a,#0x01
+	sjmp	00127$
+00125$:
+	add	a,acc
+00127$:
+	djnz	b,00125$
+	mov	_tmp0,a
+;	preemptive.c:264: do
+00105$:
+;	preemptive.c:266: current_T++;
+	mov	a,_current_T
+	inc	a
+	mov	_current_T,a
+;	preemptive.c:267: tmp0 <<= 1;
+	mov	a,_tmp0
+	mov	r7,a
+	add	a,acc
+	mov	_tmp0,a
+;	preemptive.c:268: if (current_T==4){
+	mov	a,#0x04
+	cjne	a,_current_T,00102$
+;	preemptive.c:269: current_T = 0;
+	mov	_current_T,#0x00
+;	preemptive.c:270: tmp0 = 1;
+	mov	_tmp0,#0x01
+00102$:
+;	preemptive.c:272: if (bitmap&tmp0){
+	mov	a,_tmp0
+	anl	a,_bitmap
+	jz	00105$
+;	preemptive.c:276: RESTORESTATE;
+	mov	a,_current_T
+	add	a,#_T_SP
+	mov	r1,a
+	mov	_SP,@r1
+	POP PSW 
+	POP DPH 
+	POP DPL 
+	POP B 
+	POP A 
+;	preemptive.c:277: EA = 1;
+;	assignBit
+	setb	_EA
+;	preemptive.c:278: }
+	ret
+;------------------------------------------------------------
+;Allocation info for local variables in function 'myTimer0Handler'
+;------------------------------------------------------------
+;	preemptive.c:280: void myTimer0Handler(void)
+;	-----------------------------------------
+;	 function myTimer0Handler
+;	-----------------------------------------
+_myTimer0Handler:
+;	preemptive.c:282: EA = 0;
+;	assignBit
+	clr	_EA
+;	preemptive.c:283: SAVESTATE;
+	PUSH A 
+	PUSH B 
+	PUSH DPL 
+	PUSH DPH 
+	PUSH PSW 
+	mov	a,_current_T
+	add	a,#_T_SP
+	mov	r0,a
+	mov	@r0,_SP
+;	preemptive.c:284: tmp0 = 1<<current_T;
+	mov	r7,_current_T
+	mov	b,r7
+	inc	b
+	mov	a,#0x01
+	sjmp	00124$
+00122$:
+	add	a,acc
+00124$:
+	djnz	b,00122$
+	mov	_tmp0,a
+;	preemptive.c:285: do
+00105$:
+;	preemptive.c:297: current_T++;
+	mov	a,_current_T
+	inc	a
+	mov	_current_T,a
+;	preemptive.c:298: tmp0 <<= 1;
+	mov	a,_tmp0
+	mov	r7,a
+	add	a,acc
+	mov	_tmp0,a
+;	preemptive.c:299: if (current_T==4){
+	mov	a,#0x04
+	cjne	a,_current_T,00102$
+;	preemptive.c:300: current_T = 0;
+	mov	_current_T,#0x00
+;	preemptive.c:301: tmp0 = 1;
+	mov	_tmp0,#0x01
+00102$:
+;	preemptive.c:303: if (bitmap&tmp0){
+	mov	a,_tmp0
+	anl	a,_bitmap
+	jz	00105$
+;	preemptive.c:307: RESTORESTATE;
+	mov	a,_current_T
+	add	a,#_T_SP
+	mov	r1,a
+	mov	_SP,@r1
+	POP PSW 
+	POP DPH 
+	POP DPL 
+	POP B 
+	POP A 
+;	preemptive.c:308: EA = 1;
+;	assignBit
+	setb	_EA
+;	preemptive.c:311: __endasm;
+	RETI
+;	preemptive.c:312: }
+	ret
+	.area CSEG    (CODE)
+	.area CONST   (CODE)
+	.area XINIT   (CODE)
+	.area CABS    (ABS,CODE)
